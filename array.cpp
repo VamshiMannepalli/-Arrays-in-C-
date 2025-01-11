3c7da9c61ce7dca98facf52112c82decb26c5eaf
VamshiMannepalli <vamshi.mannepalli@cuw.edu>
2025-01-11 13:29:57 -0600

    array.cpp

--- array.cpp
+++ array.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int*  targetsum(int nums[], int target) {
-    static int result_array[2];
-    for(int i =0; i< sizeof(nums)-1; i++)
-    {
-        for(int j=i+1; j< sizeof(nums)-1; j++){
-            if( nums[i]+ nums[j] == target){
-                result_array[0] = i;
-                result_array[1] = j;
-                return result_array;
-                       }
+vector<int> targetSum(const int nums[], int size, int target) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (nums[i] + nums[j] == target) {
+                return {i, j}; // Return the indices as a vector
+            }
         }
     }
-    return nullptr;
+    return {}; // Return an empty vector if no solution is found
 }
 
-int main(){
-    int nums[] = {2,7,11,15};
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int size = sizeof(nums) / sizeof(nums[0]); // Calculate the size of the array
     int target = 9;
-    int* target_indexes = targetsum(nums, target);
 
-    if(target_indexes != nullptr) {
-        cout << "output is: [" << target_indexes[0] << ", " << target_indexes[1] << "]" << endl;
+    vector<int> result = targetSum(nums, size, target);
+
+    if (!result.empty()) {
+        cout << "Output is: [" << result[0] << ", " << result[1] << "]" << endl;
+    } else {
+        cout << "No solution found!" << endl;
     }
-    else{
-        cout << "NO solution found!" << endl;
-    }
+
     return 0;
 }
